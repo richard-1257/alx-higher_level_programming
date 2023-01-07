88fe51c4805038f020bc6df9cee5411ca0cf4867
richard <richardsifeanyi@gmail.com>
2023-01-07 15:56:31 -0800

    update function that remove charaters from string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-def no_c(my_string):
-    new = ""
-    for char in my_string:
-        if char not in "cC":
-            new += char
 
-            return new
+def no_c(my_string):
+    new_s = my_string.translate({ord('c'): None})
+    new_s = new_s.translate({ord('C'): None})
+    return new_s
