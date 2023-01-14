d0b9632672498d12f2e42f000c90771b40f05f22
richard <richardsifeanyi@gmail.com>
2023-01-13 18:37:34 -0800

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
 
-    if len(a_dictionary.value()) == 0:
+    if len(a_dictionary.values()) == 0:
         return None
 
-    max_value = max(a_dictionary.value())
+    max_value = max(a_dictionary.values())
 
     for key, value in a_dictionary.items():
         if value == max_value:
