d6dfcddace8172abb7173abde9480441d2a06076
richard <richardsifeanyi@gmail.com>
2023-01-13 16:39:11 -0800

    value of matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-
 def square_matrix_simple(matrix=[]):
     new_matrix = []
 
     for i in matrix:
-        new_matrix.append(list(map(lamboda x: x**2, i)))
+        new_matrix.append(list(map(lambda x: x**2, i)))
 
     return new_matrix
