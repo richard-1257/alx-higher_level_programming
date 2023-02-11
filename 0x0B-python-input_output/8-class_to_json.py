4b1591f7e83a1b427abaabcfec1a341e6fcc8e7f
richard <richardsifeanyi@gmail.com>
2023-02-11 04:16:30 -0800

    pycode

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,11 +1,13 @@
-#!/usr/bib/python3
-"""
-Contains the "class_to_json" function
+#!/usr/bin/python3
+""" Module that returns the dictionary description with a simple
+data structure for a JSON serialization of an object
 """
 
 
 def class_to_json(obj):
-    """returns the dictionary description with simple data structure
-    (list, dictionary, string, integer and boolean)
-    for JSON serialization of an object"""
-    return obj.__dict__
+    """ Function that retuns the dictionary description of an obj """
+
+    res = {}
+    if hasattr(obj, "__dict__"):
+        res = obj.__dict__.copy()
+    return res
--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-"""
-Contain the class "Student"
+""" Module that defines the class Student
 """
 
 
-class student:
-    """Representation of a student"""
+class Student:
+    """ Class to create student instances """
+
     def __init__(self, first_name, last_name, age):
-        """Initializes the student"""
+        """ Special method to initialize """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """returns a dictionary representation of a student instance"""
-        return self.__dict__
+        """ Method that returns directory description """
+        return self.__dict__.copy()
