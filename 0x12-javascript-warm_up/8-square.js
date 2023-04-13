901670111f8f531d3adf192614f90d03bab6d826
richard <richardsifeanyi@gmail.com>
2023-04-13 03:57:02 -0700

    square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-const args = process.argv.slice(2);
-const x = parseInt(args[0]);
-
-if (isNaN(x)) {
-  console.log("Missing number of occurrences");
+const size = Math.floor(Number(process.argv[2]));
+if (isNaN(size)) {
+  console.log('Missing size');
 } else {
-  for (let i = 0; i < x; i++) {
-    console.log("C is fun");
+  for (let r = 0; r < size; r++) {
+    let row = '';
+    for (let c = 0; c < size; c++) row += 'X';
+    console.log(row);
   }
 }
