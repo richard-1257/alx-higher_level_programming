47e92af0a25204f23f09d927dd93b84486e9d25b
richard <richardsifeanyi@gmail.com>
2023-04-13 02:31:35 -0700

    argument

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,13 +1,3 @@
 #!/usr/bin/node
-const args = process.argv.slice(2);
-
-switch (args.length) {
-  case 0:
-    console.log("No argument");
-    break;
-  case 1:
-    console.log("Argument found");
-    break;
-  default:
-    console.log("Arguments found");
-}
+const count = process.argv.length;
+console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
